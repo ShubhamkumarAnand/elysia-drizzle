b311f7ba96191027e723b2b1213c6b8ffde6393b
imskanand <the.anand3099@gmail.com>
2023-09-08 00:39:09 +0530

    playing with elysia bun framework

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+function signIn(body: unknown) {
+  console.log(body)
+}
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .post("/sign-up", ({ body }) => ({
+    message: `${body.username}`
+  }), {
+    body: t.Object({
+      username: t.String(),
+      password: t.String(),
+      email: t.String()
+    })
+  })
+  .post("/log-in", ({ body }) => signIn(body), {
+    body: t.Object({
+      username: t.String(),
+      password: t.String()
+    })
+  })
+  .get("/dashboard/:name", (({ params: { name } }) => ({
+    message: `Here is your dashboard with ${name}`
+  })))
+  .listen(3005)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
