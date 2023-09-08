978101e9053c08602ab7b4b394aa0d6346408d4e
imskanand <the.anand3099@gmail.com>
2023-09-08 13:06:49 +0530

    drizzle + elysia integration

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,57 @@
 import { Elysia, t } from "elysia";
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from "postgres";
+import { users } from "../drizzle/schema";
 
-function signIn(body: unknown) {
-  console.log(body)
+const dbUrl = process.env.RAILWAY_DB_URL as string
+
+const client = postgres(dbUrl)
+const db = drizzle(client)
+
+const allUser = await db.select().from(users);
+
+interface body {
+  id?: number
+  full_name: string,
+  phone: string
+};
+
+async function signIn(body: body) {
+  const allUsers = await db.select().from(users)
+  const getUser = allUsers.find(user => user.full_name === body.full_name)
+  if (getUser) {
+    return `You are logged in successfully.`
+  }
+  return `Invalid credentials`
 }
+
+async function signUp(body: body) {
+  await db.insert(users).values({ id: body.id, full_name: body.full_name, phone: body.phone })
+  return `You are signed Up`
+}
+
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
-  .post("/sign-up", ({ body }) => ({
-    message: `${body.username}`
-  }), {
+  .get("/users", () => ({
+    users: allUser
+  }))
+  .post("/sign-up", ({ body }) => signUp(body), {
     body: t.Object({
-      username: t.String(),
-      password: t.String(),
-      email: t.String()
+      id: t.Number(),
+      full_name: t.String(),
+      phone: t.String()
     })
   })
-  .post("/log-in", ({ body }) => signIn(body), {
+  .post("/sign-in", ({ body }) => signIn(body), {
     body: t.Object({
-      username: t.String(),
-      password: t.String()
+      full_name: t.String(),
+      phone: t.String()
     })
   })
   .get("/dashboard/:name", (({ params: { name } }) => ({
     message: `Here is your dashboard with ${name}`
   })))
-  .listen(3005)
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
